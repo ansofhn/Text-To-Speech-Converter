c858cd48de194685ebf876d95a61f87160fc99cd
Ansof Habibunnadjar <59586887+ansofhn@users.noreply.github.com>
2022-06-08 20:39:58 +0700

    Add files via upload

---
+++ speechScript.js
@@ -0,0 +1,38 @@
+const textarea = document.querySelector("textarea"),
+  voiceList = document.querySelector("select"),
+  speechButton = document.querySelector("button");
+
+let synth = speechSynthesis;
+
+voices();
+
+function voices() {
+  for (let voice of synth.getVoices()) {
+    let selected = voice.name === "Google US English" ? "selected" : "";
+    let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
+    voiceList.insertAdjacentHTML("beforeend", option);
+  }
+}
+
+synth.addEventListener("voiceschanged", voices);
+
+function textToSpeech(text) {
+  let utterance = new SpeechSynthesisUtterance(text);
+  for (let voice of synth.getVoices()) {
+    if (voice.name === voiceList.value) {
+      utterance.voice = voice;
+    }
+  }
+  synth.speak(utterance);
+}
+
+speechButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (textarea.value !== "") {
+    if (!synth.speaking) {
+      textToSpeech(textarea.value);
+    } else {
+      speechButton.innerText = "Convert To Speech";
+    }
+  }
+});
